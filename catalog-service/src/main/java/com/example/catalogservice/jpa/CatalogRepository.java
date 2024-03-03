e3cf4686e528686d0d231120163d1b1669f5a243
YouchanKim <dbcks4646@eco.co.kr>
2024-03-03 22:38:54 +0900

    카테고리 조회 서비스 생성 및 gateway 등록

---
+++ catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
@@ -0,0 +1,6 @@
+package com.example.catalogservice.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
+}
